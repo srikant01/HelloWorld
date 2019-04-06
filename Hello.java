d33816f4988df4c2ca5db894fa88b62b48c47b84
sri <dk.srikant@gmail.com>
2019-04-05 21:48:13 -0700

    commited;

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
+
 public class Hello{
 	public static void main(String [] args){
 
-	for (int i=0; i <6; i++){
-	System.out.println("Hello World..." + i);
-	}
+		
+		 for (int i=0; i <6; i++){ System.out.println("Hello World..." + i); }
+		 
 	}
 	
 }
